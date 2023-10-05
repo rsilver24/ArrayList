bf482aebb4c99505f21014b92ff3ebfdb8cba038
Roan Silver <rsilver24@rsilver24.kentdenver.org>
2023-10-05 10:07:20 -0600

    Version with Method Layouts and partly-coded 'add' Method

--- ArrayList/src/ArrayList.java
+++ ArrayList/src/ArrayList.java
@@ -1,3 +1,38 @@
-public class ArrayList {
-    
+public class ArrayList<T> {
+
+    private Node value1;
+    private int size;
+
+    public ArrayList(){
+        size = 0;
+    }
+
+    public boolean add(T data){
+        Node newValue = new Node();
+        Node subNode = new Node();
+        newValue.setValue(data);
+        for (int i = 0; i <= size(); i++){
+            newValue.setParentNode(newValue.getChildNode());
+        }
+    }
+
+    public void add(int i, T data){
+
+    }
+
+    public T remove(int i){
+
+    }
+
+    public T get(int i){
+
+    }
+
+    public T set(int i, T data){
+
+    }
+
+    public int size(){
+        return size;
+    }
 }
