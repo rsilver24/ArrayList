af5727c4d1406810eead600d480733c5ce3d28db
Roan Silver <rsilver24@rsilver24.kentdenver.org>
2023-10-05 14:58:17 -0600

    Version with a finished default add method

--- ArrayList/src/ArrayList.java
+++ ArrayList/src/ArrayList.java
@@ -1,36 +1,63 @@
 public class ArrayList<T> {
 
+    // Instance Data
+
     private Node value1;
     private int size;
+
+    // Default Constructor
 
     public ArrayList(){
         size = 0;
     }
 
+    // Methods
+
+    // Default Add Method
+
     public boolean add(T data){
         Node newValue = new Node();
-        Node subNode = new Node();
         newValue.setValue(data);
-        for (int i = 0; i <= size(); i++){
-            newValue.setParentNode(newValue.getChildNode());
+
+        if (size == 0){
+            value1 = newValue;
+        } else if (size == 1){
+            value1.setChildNode(newValue);
+            newValue.setParentNode(value1);
+        } else{
+            newValue.setParentNode(value1);
+            for (int i = 1; i < size(); i++){
+                newValue.setParentNode(newValue.getParentNode().getChildNode());
+            } newValue.getParentNode().setChildNode(newValue);
         }
+        size++;
+        return true;
     }
 
-    public void add(int i, T data){
+    // Add @ Index Method
 
-    }
-
-    public T remove(int i){
-
-    }
-
-    public T get(int i){
+    public void add(int index, T data){
 
     }
 
-    public T set(int i, T data){
+    // Remove Method
+
+    public T remove(int index){
 
     }
+
+    // Get Method
+
+    public T get(int index){
+    }
+
+    // Set Method
+
+    public T set(int index, T data){
+
+    }
+
+    // Size Method
 
     public int size(){
         return size;
